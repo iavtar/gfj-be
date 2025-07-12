d574c66575a475226ea55184205b59f5e1c4b2da
iavtar <indra.avatarsingh@gmail.com>
2025-07-12 21:33:18 +0530

    bootstrapped project

---
+++ src/test/java/com/iavtar/gfj_be/GfjBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iavtar.gfj_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GfjBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
