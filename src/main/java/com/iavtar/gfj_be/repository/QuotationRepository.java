ee452ee2ecf9a99abd3bb0ed07f73478fa4c03cb
ashutoshpandey27 <ashutoshp2706@gmail.com>
2025-07-27 22:29:02 +0530

    added getQuotationsByClient and getQuotationsByAgent

--- src/main/java/com/iavtar/gfj_be/repository/QuotationRepository.java
+++ src/main/java/com/iavtar/gfj_be/repository/QuotationRepository.java
@@ -12,4 +12,9 @@ public interface QuotationRepository extends JpaRepository<Quotation, Long> {
     Iterable<Long> id(Long id);
 
     Page<Quotation> findAllByClientId(Long clientId, Pageable pageable);
+
+    Page<Quotation> findAllByAgentId(Long agentId, Pageable pageable);
+
+    Page<Quotation> findAllByClientIdAndAgentId(Long clientId, Long agentId, Pageable pageable);
+
 }
