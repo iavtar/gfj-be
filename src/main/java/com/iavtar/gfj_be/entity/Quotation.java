bae87b8ebea3cc7e43db8c85e580df3529f524f7
iavtar <indra.avatarsingh@gmail.com>
2025-08-03 01:34:28 +0530

    added description field in quotation

--- src/main/java/com/iavtar/gfj_be/entity/Quotation.java
+++ src/main/java/com/iavtar/gfj_be/entity/Quotation.java
@@ -23,6 +23,8 @@ public class Quotation {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String description;
+
     @Lob
     @Column(columnDefinition = "TEXT")
     private String data;
