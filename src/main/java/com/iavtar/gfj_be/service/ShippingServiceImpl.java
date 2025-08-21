37055a48a23e9e503a1e416c6ebbb3141f8f8b2d
iavtar <indra.avatarsingh@gmail.com>
2025-08-22 02:05:22 +0530

    updated code for shipping

--- src/main/java/com/iavtar/gfj_be/service/ShippingServiceImpl.java
+++ src/main/java/com/iavtar/gfj_be/service/ShippingServiceImpl.java
@@ -15,7 +15,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 
 import java.util.*;
 import java.util.stream.Collectors;
